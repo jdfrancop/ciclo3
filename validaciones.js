e09c750cade4cfd30d43711b77dd121713f7ddb1
Nick Andres Piragua <nickan1992@hotmail.es>
2021-10-07 12:06:53 -0500

    Subida de archvivo validaciones js para sprint 2 NICK

---
+++ validaciones.js
@@ -0,0 +1,19 @@
+
+function validar_nombre_usuario(string)
+{      
+
+}
+
+function validar_contrasena(string)
+{   
+
+}
+
+function confirmar_contrasena(stringA, StringB)
+{
+
+}
+
+module.expots.validar_nombre_usuario=validar_nombre_usuario;
+module.expots.validar_contrasena=validar_contrasena;
+module.expots.confirmar_contrasena=confirmar_contrasena;
